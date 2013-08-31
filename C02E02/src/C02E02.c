fca6ab6f327357955431fb618a1d6085d8aa3b73
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2013-08-31 17:44:31 +0200

    first commit

---
+++ C02E02/src/C02E02.c
@@ -0,0 +1,18 @@
+/**
+ * \file C02E02.c
+ * \author Henrik Samuelsson
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * \brief Prints an address.
+ * \return 0 upon successful execution.
+ */
+int main(void) {
+	printf("Santa Claus\n");
+	printf("Santa Claus Village\n");
+	printf("FIN-96930 Arctic Circle\n");
+
+	return EXIT_SUCCESS;
+}
