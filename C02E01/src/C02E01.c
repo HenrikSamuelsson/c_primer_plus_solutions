58f6206b44b2007936e53a5be2e61cdfdc70a45d
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2013-08-31 16:33:47 +0200

    initial version

---
+++ C02E01/src/C02E01.c
@@ -0,0 +1,19 @@
+/**
+ * \file C02E01.c
+ * \author Henrik Samuelsson
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * \brief Prints a name a couple of times.
+ * \return 0 upon successful execution.
+ */
+int main(void) {
+	printf("Henrik Samuelsson\n");
+	printf("Henrik\nSamuelsson\n");
+	printf("Henrik ");
+	printf("Samuelsson");
+
+	return EXIT_SUCCESS;
+}
