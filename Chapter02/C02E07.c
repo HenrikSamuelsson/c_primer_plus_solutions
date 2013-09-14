5131f2b1ffa52a0fb566d66e90e8381dce9d581d
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2013-09-14 22:37:39 +0200

    First commit of exercise 2 in chapter 7

---
+++ Chapter02/C02E07.c
@@ -0,0 +1,40 @@
+/**
+ * \file C02E07.c
+ * \author Henrik Samuelsson
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+void oneThree(void);
+void two(void);
+
+/**
+ * \brief Prints a couple of strings with aid of helper functions.
+ * \return 0 upon successful execution.
+ */
+int main(void)
+{
+	printf("starting now:\n");
+	oneThree();
+	printf("done!\n");
+}
+
+/**
+ * \brief Prints one two three.
+ * \details Prints "one" and "three" by it self, a function is called that
+ *          prints "two".
+ */
+void oneThree(void)
+{
+	printf("one\n");
+	two();
+	printf("three\n");
+}
+
+/**
+ * \brief Prints "two".
+ */
+void two(void)
+{
+	printf("two\n");
+}
