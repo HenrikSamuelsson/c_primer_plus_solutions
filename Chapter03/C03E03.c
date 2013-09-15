19f4e62246c95ecf4bd348646abd801360f03503
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2013-09-15 19:41:25 +0200

    Committing first version of exercise 3 in chapter 3.

---
+++ Chapter03/C03E03.c
@@ -0,0 +1,17 @@
+/**
+ * \file C03E03.c
+ * \author Henrik Samuelsson
+  */
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * \brief Sounds an alert prints a string.
+ * \return 0 upon successful execution.
+ */
+int main(void) {
+	printf("\a\n");	// Makes the computer make a sound.
+	printf("Startled by the sudden sound, Sally shouted, \"By the great "
+			"Pumpkin, what was that!\"");
+	return EXIT_SUCCESS;
+}
