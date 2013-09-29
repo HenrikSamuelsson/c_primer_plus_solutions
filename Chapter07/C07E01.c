32caabd88c28c6ff506aa592c4820aa41d71807d
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2013-09-29 21:57:30 +0200

    Committing exercise 1 from chapter 7

---
+++ Chapter07/C07E01.c
@@ -0,0 +1,40 @@
+/**
+ * \file C07E01.c
+ * \author Henrik Samuelsson
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * \brief Counts different types of characters in the user input.
+ * \note It is necessary to press enter after the #. The reason for
+ *       this is probably because the input stream is buffered and
+ *       enter will flush the buffer.
+ * \return 0 upon successful execution.
+ */
+int main(void)
+{
+	int newlines, spaces, others;
+	char ch;
+
+	newlines = 0;
+	spaces = 0;
+	others = 0;
+
+	// Check what the user enters until a # is found.
+	while ((ch = getchar()) != '#')
+	{
+		if(ch == '\n')
+			newlines++;
+		else if(ch == ' ')
+			spaces++;
+		else
+			others++;
+	}
+
+	// Present data about the input.
+	printf("Found %d newlines, %d spaces and %d other characters.",
+			newlines, spaces, others);
+
+	return EXIT_SUCCESS;
+}
