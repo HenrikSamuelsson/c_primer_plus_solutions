f39d9991c16228720e91b745ba9becd0b38c4e2a
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2013-10-03 23:30:24 +0200

    Committing Exercise 3 from chapter 6

---
+++ Chapter06/C06E03.c
@@ -0,0 +1,29 @@
+/**
+ * \file C06E07.c
+ * \author Henrik Samuelsson
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * \brief Prints a letter pattern using nested loops.
+ * \note Assumes that a character encoding with sequential codes for the letters
+ *       A to F is used on the system where the code will run.
+ * \return 0 upon successful execution.
+ */
+int main(void)
+{
+	char start, end, current;
+
+	start = 'F';
+
+	for(end = 'F'; end >= 'A'; end--)
+	{
+		for(current = start; current >= end; current--)
+			printf("%c", current);
+		printf("\n");
+	}
+
+	return EXIT_SUCCESS;
+}
