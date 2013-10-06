c910d84537371b810b09e2ce288ea863ce38b828
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2013-10-06 20:28:17 +0200

    Committing exercise 6 from chapter 6.

---
+++ Chapter06/C06E06.c
@@ -0,0 +1,24 @@
+/**
+ * \file C06E06.c
+ * \author Henrik Samuelsson
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * \brief Takes a word as input and prints it backwards;
+ * \return 0 upon successful execution.
+ */
+int main(void)
+{
+	char word[40];    // The word that the user enters.
+	int length;       // The length of the word.
+
+	printf("Enter a word: ");
+	scanf("%s", &word[0]);
+	for(length = strlen(word); length >= 0; length--)
+			printf("%c", word[length]);
+
+	return EXIT_SUCCESS;
+}
