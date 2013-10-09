bc1716c8b25875ed15215bc2dea507318aebbef6
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2013-10-09 23:07:17 +0200

    Committing exercise 9 from chapter6

---
+++ Chapter06/C06E09.c
@@ -0,0 +1,32 @@
+/**
+ * \file C06E09.c
+ * \author Henrik Samuelsson
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * \brief Mathematical summation numbers in a range.
+ * \return 0 upon successful execution.
+ */
+int main(void)
+{
+	int in1, in2, index, result;
+
+	printf("Enter lower and upper integer limits: ");
+	scanf("%d %d", &in1, &in2);
+	while( (in2 - in1) > 0 )
+	{
+		result=0;
+		for(index = in1; index <= in2; index++)
+			result += index * index;
+		printf("The sums of the squares from %d to %d is %d\n"
+				, in1 * in1, in2 * in2, result);
+		printf("Enter next set of limits: ");
+		scanf("%d %d", &in1, &in2);
+	}
+	printf("Done");
+
+	return EXIT_SUCCESS;
+}
