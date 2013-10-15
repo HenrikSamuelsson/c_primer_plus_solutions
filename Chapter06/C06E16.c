ce7b24e0cb816b18b59f3319a5d494f822432005
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2013-10-15 22:10:13 +0200

    Committing exercise 16 from chapter 6.

---
+++ Chapter06/C06E16.c
@@ -0,0 +1,31 @@
+/**
+ * \file C06E15.c
+ * \author Henrik Samuelsson
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * \brief Calculation of how long an amount of money will last, based on
+ *        interest and yearly withdrawals.
+ * \return 0 upon successful execution.
+ */
+int main(void)
+{
+	const double START_CAPITAL = 1.0E6;
+	const double INTEREST_RATE = 0.08;
+	const double YEARLY_WITHDRAWAL = 0.1E6;
+	double capital;
+	int yearsPassed;
+
+
+	capital = START_CAPITAL;
+	while(capital > 0)
+	{
+		capital = capital + capital * INTEREST_RATE - YEARLY_WITHDRAWAL;
+		yearsPassed++;
+	}
+	printf("Chuckie's money is gone after %d years.", yearsPassed);
+
+	return EXIT_SUCCESS;
+}
