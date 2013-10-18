8e0ce4cde023d189e2a51db468f8a10e79f11d58
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2013-10-18 20:40:19 +0200

    Committing exercise 4 from chapter 7.

---
+++ Chapter07/C07E04.c
@@ -0,0 +1,45 @@
+/**
+ * \file C07E04.c
+ * \author Henrik Samuelsson
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+#define STOP '#'
+
+/**
+ * \brief Takes characters as input and replaces some characters.
+ * \note The instructions for ending the program are aimed a Windows 7 users,
+ *       might function differently on other systems.
+ * \return 0 upon successful execution.
+ */
+int main(void)
+{
+	char ch;
+	int dotCount = 0;
+	int exlCount = 0;
+	printf("Input some characters\nEnd the input with # followed by the enter "
+		   "key.\n");
+	while ((ch = getchar()) != STOP)
+	{
+	    if (ch == '.')
+	    {
+	    	putchar('!');
+	    	dotCount++;
+	    }
+	    else if (ch == '!')
+	    {
+	    	putchar('!');
+	    	putchar('!');
+	    	exlCount++;
+	    }
+	    else
+	    	putchar(ch);
+	}
+	printf("Replaced periods with exclamations marks on %d places.\n"
+			, dotCount);
+	printf("Replaced a exclamation mark with two exclamations marks on %d "
+			"places.\n", exlCount);
+
+	return EXIT_SUCCESS;
+}
