25aa86c1ebd9ee2abfc2d3fc6d6aa9a9418bc397
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2013-10-23 22:09:24 +0200

    Committing again with correct source file.

--- Chapter07/C07E06.c
+++ Chapter07/C07E06.c
@@ -1,17 +1,34 @@
-/*
- ============================================================================
- Name        : C07E06.c
- Author      : 
- Version     :
- Copyright   : Your copyright notice
- Description : Hello World in C, Ansi-style
- ============================================================================
+/**
+ * \file C07E06.c
+ * \author Henrik Samuelsson
  */
-
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+#define STOP '#'
+#define FIRST 'e'
+#define SECOND 'i'
+
+/**
+ * \brief Counts numbers of occurrences of a repeating pattern in a text.
+ * \return 0 upon successful execution.
+ */
+int main(void)
+{
+	int count = 0;
+	char previousChar = 0;
+	char currentChar;
+
+	printf("Enter a text (# to end input): ");
+	while((currentChar = getchar()) != STOP )
+	{
+		if(previousChar == FIRST && currentChar == SECOND)
+			count++;
+		previousChar = currentChar;
+	}
+	printf("The letter combination \"%c%c\" was found %d times.",
+			FIRST, SECOND, count);
+
 	return EXIT_SUCCESS;
 }
