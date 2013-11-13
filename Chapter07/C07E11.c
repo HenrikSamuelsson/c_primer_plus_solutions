10e1e3074463a74586510fb0c172e9492f19bb71
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2013-11-13 23:48:34 +0100

    Committing complete version of exercise 11 from chapter 7.

--- Chapter07/C07E11.c
+++ Chapter07/C07E11.c
@@ -6,18 +6,20 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-#define ARTICHOKES_PRICE      1.25
-#define BEETS_PRICE           0.65
+#define ARTICHOKE_PRICE       1.25
+#define BEET_PRICE            0.65
 #define CARROT_PRICE          0.89
 #define DISCOUNT_PERCENTAGE   0.05
-#define SHIPPING_LOW          3.50
+#define MEDIUM_LIMIT          5.00
+#define LARGE_LIMIT           20.00
+#define SHIPPING_SMALL        3.50
 #define SHIPPING_MEDIUM       10.00
 #define SHIPPING_LARGE        8.00
-#define SHIPPING_LARGE_WEIGHT 0.10
+#define SHIPPING_LARGE_FACTOR 0.10
+#define DISCOUNT_LIMIT        100
 
 /**
  * \brief Vegetable ordering software.
- * \note TODO not finished.
  * \warning Not really a good idea in reality to use doubles when counting
  *          money because of trouble with rounding errors.
  * \return 0 upon successful execution.
@@ -25,11 +27,14 @@
 int main(void)
 {
 	char choice;
-	bool orderDone, validChoice, validAmount;
+	bool orderDone, validChoice, discount;
 	double temp;
-	double artichokes = 0;
-	double beets = 0;
-	double carrots = 0;
+	double artichokesWeight = 0;
+	double beetsWeight = 0;
+	double carrotsWeight = 0;
+	double totalWeight;
+	double artichokesCost, beetsCost, carrotsCost, totalBeforeDiscount,
+	       totalCostAfterDiscount, discountAmount, shipping, grandTotal;
 
 	printf("***************************************************************\n");
 	printf("\n");
@@ -97,15 +102,15 @@ int main(void)
 			{
 			case 'a':
 			case 'A':
-				artichokes = temp;
+				artichokesWeight = temp;
 				break;
 			case 'b':
 			case 'B':
-				beets = temp;
+				beetsWeight = temp;
 				break;
 			case 'c':
 			case 'C':
-				carrots = temp;
+				carrotsWeight = temp;
 				break;
 			}
 		printf("Make another choice from the menu: ");
@@ -113,11 +118,58 @@ int main(void)
 	}
 
 	// User is done ordering. Calculate and present the cost of the order.
-	printf("\nARTICHOKES\n");
-	printf("    cost per pound  %20.2lf\n", ARTICHOKES_PRICE);
-	printf("    pounds ordered  %20.2lf\n", artichokes);
+	artichokesCost = artichokesWeight * ARTICHOKE_PRICE;
+	beetsCost = beetsWeight * BEET_PRICE;
+	carrotsCost = carrotsWeight * CARROT_PRICE;
+	totalBeforeDiscount = artichokesCost + beetsCost + carrotsCost;
+	if(totalBeforeDiscount >=  DISCOUNT_LIMIT)
+	{
+		discount = true;
+		discountAmount = DISCOUNT_PERCENTAGE * totalBeforeDiscount;
+		totalCostAfterDiscount = totalBeforeDiscount - discount;
+	}
+	else
+	{
+		discount = false;
+		totalCostAfterDiscount = totalBeforeDiscount;
+	}
+	totalWeight = artichokesWeight + beetsWeight + carrotsWeight;
+	if(totalCostAfterDiscount >= LARGE_LIMIT)
+	{
+		shipping = SHIPPING_LARGE + SHIPPING_LARGE_FACTOR * totalWeight;
+	}
+	else if(totalCostAfterDiscount > MEDIUM_LIMIT)
+	{
+		shipping = SHIPPING_MEDIUM;
+	}
+	else
+	{
+		shipping = SHIPPING_SMALL;
+	}
+	grandTotal = totalCostAfterDiscount + shipping;
 
-	getchar();
+	printf("\nArtichokes:\n");
+	printf("cost per pound  %20.2lf\n", ARTICHOKE_PRICE);
+	printf("pounds ordered  %20.2lf\n", artichokesWeight);
+	printf("cost            %20.2lf\n", artichokesCost);
+
+	printf("\nBeets:\n");
+	printf("cost per pound  %20.2lf\n", BEET_PRICE);
+	printf("pounds ordered  %20.2lf\n", beetsWeight);
+	printf("cost            %20.2lf\n", beetsCost);
+
+	printf("\nCarrots:\n");
+	printf("cost per pound  %20.2lf\n", CARROT_PRICE);
+	printf("pounds ordered  %20.2lf\n", carrotsWeight);
+	printf("cost            %20.2lf\n", carrotsCost);
+
+	printf("\nTotal           %20.2lf\n", totalBeforeDiscount);
+	if(discount)
+	{
+		printf("Discount        %20.2lf\n", -discountAmount);
+	}
+	printf("Shipping        %20.2lf\n", shipping);
+	printf("Grand total     %20.2lf\n", grandTotal);
 
 	return EXIT_SUCCESS;
 }
