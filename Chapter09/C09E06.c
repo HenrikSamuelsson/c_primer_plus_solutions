243df906de3b4d12a4e523f2a0c8f8a9df9890a5
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2013-11-15 22:57:07 +0100

    Committing part of exercise 6 from chapter 9.

---
+++ Chapter09/C09E06.c
@@ -0,0 +1,18 @@
+/**
+ * \file C09E06.c
+ * \author Henrik Samuelsson
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * \brief Shell for this exercise.
+ * \note Not finished...
+ * \return 0 upon successful execution.
+ */
+int main(void)
+{
+
+	return EXIT_SUCCESS;
+}
